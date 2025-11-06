d366ed2352b526fc1628a66027f784e036906a58
Hoàng Diệu Linh <dieulinh120411@gmail.com>
2025-11-07 01:43:12 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\admin\GroupController;
 use App\Http\Controllers\admin\UserController;
+use App\Http\Controllers\client\UserController as ClientController;
 use App\Http\Livewire\LabCalendar;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeControler;
@@ -9,12 +11,16 @@ use App\Http\Controllers\Auth\AuthenticateController;
 Route::get('auth/redirect',[AuthenticateController::class,'redirectToSSO'])->name('sso.redirect');
 Route::get('auth/callback', [AuthenticateController::class,'handleSSOCallback'])->name('sso.callback');
 Route::post('/logout', [AuthenticateController::class, 'logout'])->name('handleLogout');
-
 //Route::get('/', function () {
 // })->name('home');
 Route::get('/', LabCalendar::class )->name('home');;
 Route::get('/api/bookings', [LabCalendar::class, 'getAllBookings']);
+
 Route::get('/event-calendar', [HomeControler::class, 'eventsCalendar'])->name('events.calendar');
+
+Route::middleware('checkAuth')->group(function () {
+    Route::get('/information-user',[ClientController::class,'infoUser'])->name('client.info-user');
+});
 
 Route::middleware('role:admin')->group(function () {
     Route::prefix('admin')->group(function () {
@@ -25,6 +31,12 @@ Route::middleware('role:admin')->group(function () {
         Route::prefix('users')->group(function () {
             Route::get('/', [UserController::class, 'index'])->name('admin.users.index');
             Route::get('/edit/{id}', [UserController::class, 'edit'])->name('admin.users.edit');
+        });
+
+        Route::prefix('groups')->group(function () {
+            Route::get('/',[GroupController::class, 'index'] )->name('admin.groups.index');
+            Route::get('/create',[GroupController::class, 'create'] )->name('admin.groups.create');
+            Route::get('/edit/{id}',[GroupController::class, 'edit'] )->name('admin.groups.edit');
         });
     });
 });
