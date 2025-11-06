e5db6e4a6f4d91f210c09962f3cdc3b3fd1387a9
xphongmaru <671762@sv.vnua.edu.vn>
2025-11-06 09:43:14 +0700

    add middleware check logged in

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'role' => App\Http\Middleware\CheckUserRole::class,
+            'checkAuth' => App\Http\Middleware\CheckLoggedIn::class,
         ])->trustProxies('*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
