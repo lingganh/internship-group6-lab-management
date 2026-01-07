40e5232290d0e33db7858435cb6e1d0b59c37788
dieulinhhoang <dieuleen1211@gmail.com>
2026-01-07 16:06:57 +0700

    fix ui

--- resources/views/livewire/lab-diary.blade.php
+++ resources/views/livewire/lab-diary.blade.php
@@ -1,6 +1,35 @@
 <div>
-  {{-- SESSION TOAST PAYLOAD (đọc từ session flash) --}}
-  <div
+  <div>
+        <div class="page-header page-header-light shadow">
+            <div class="page-header-content d-lg-flex">
+                <div class="d-flex">
+                    <h4 class="page-title mb-0">
+                        Nhật ký sử dụng
+                    </h4>
+
+                    <a href="#page_header"
+                       class="btn btn-light align-self-center collapsed d-lg-none border-transparent rounded-pill p-0 ms-auto"
+                       data-bs-toggle="collapse">
+                        <i class="ph-caret-down collapsible-indicator ph-sm m-1"></i>
+                    </a>
+                </div>
+            </div>
+            <div class="page-header-content d-lg-flex border-top">
+                <div class="d-flex">
+                    <div class="breadcrumb py-2">
+                        <a href="{{route('admin.dashboard')}}" class="breadcrumb-item"><i class="ph-house"></i></a>
+                        <span class="breadcrumb-item active">Nhật ký sử dụng </span>
+                    </div>
+
+                    <a href="#breadcrumb_elements" class="btn btn-light align-self-center collapsed d-lg-none border-transparent rounded-pill p-0 ms-auto" data-bs-toggle="collapse">
+                        <i class="ph-caret-down collapsible-indicator ph-sm m-1"></i>
+                    </a>
+                </div>
+
+            </div>
+        </div>
+    </div>
+   <div
     id="toastPayload"
     data-success="{{ session('success') }}"
     data-error="{{ session('error') }}"
