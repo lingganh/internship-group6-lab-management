00f287e1c3f2add80ae1b1c1b3e149db415d6388
dieulinhhoang <dieuleen1211@gmail.com>
2026-01-27 15:22:15 +0700

    fix calendar

---
+++ app/Mail/BatchApprovalNotification.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Support\Collection;
+
+class BatchApprovalNotification extends Mailable implements ShouldQueue
+{
+    use Queueable, SerializesModels;
+
+    public Collection $events;
+    public string $roomCode;
+
+    /**
+     * @param Collection $events  Danh sách lịch đã duyệt
+     * @param string $roomCode   Mã phòng lab
+     */
+    public function __construct(Collection $events, string $roomCode)
+    {
+        $this->events = $events;
+        $this->roomCode = $roomCode;
+    }
+
+    public function build()
+    {
+        return $this
+            ->subject('Lịch phòng lab đã được phê duyệt')
+            ->view('mail.batch-approval-notification');
+    }
+}
